0466e2e8135184b798d047e62c1a878c2f1c10aa
Youssef Ehab <youssef.ehab01@gmail.com>
2023-10-06 04:52:11 -0700

    C Programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != ('z' + 1))
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+		letter += 1;
+	}
+	putchar('\n');
+	return (0);
+}
