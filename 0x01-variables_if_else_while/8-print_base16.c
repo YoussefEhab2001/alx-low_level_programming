892cddcb154148d936a4b600a13689fcadea39b2
Youssef Ehab <youssef.ehab01@gmail.com>
2023-10-06 05:20:56 -0700

    C Programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char chr = '0';
+
+	while (chr < ('9' + 1))
+	{
+		putchar(chr);
+		chr += 1;
+	}
+	chr = 'a';
+	while (chr < ('f' + 1))
+	{
+		putchar(chr);
+		chr +=1;
+	}
+	putchar('\n');
+	return (0);
+}
