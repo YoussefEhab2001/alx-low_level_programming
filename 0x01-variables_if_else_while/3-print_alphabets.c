f4b9f651b32dbbd5eee288906d0c6189cf410e9a
Youssef Ehab <youssef.ehab01@gmail.com>
2023-10-06 04:49:52 -0700

    C Programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != ('z' + 1))
+	{
+		putchar(letter);
+		letter += 1;
+	}
+	letter = 'A';
+	while (letter != ('Z' + 1))
+	{
+		putchar(letter);
+		letter += 1;
+	}
+	putchar('\n');
+	return (0);
+}
