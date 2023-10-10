f42b13782e0c8fb31cfb210323a12bce5613f392
Youssef Ehab <youssef.ehab01@gmail.com>
2023-10-10 11:24:34 -0700

    C Programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char chr = 'a';
+
+	while (chr <= 'z')
+	{
+		_putchar(chr);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
