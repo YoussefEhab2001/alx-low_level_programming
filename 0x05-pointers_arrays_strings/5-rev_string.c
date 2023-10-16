bfc6810a559437b161694d972969598009632514
Youssef Ehab <youssef.ehab01@gmail.com>
2023-10-16 12:10:36 -0700

    C Programming

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char *tmp;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		tmp[i] = s[i];
+	}
+	for (j = 0; i >= 0; j++, i--)
+	{
+		s[j] = tmp[i];
+	}
+}
