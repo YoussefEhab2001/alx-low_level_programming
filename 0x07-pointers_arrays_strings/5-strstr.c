3130650a1a765713368b4a5bde0a9179f26acc30
Youssef Ehab <youssef.ehab01@gmail.com>
2023-10-24 10:45:12 -0700

    C Programming

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,26 +11,26 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *occurance, *tempNeedle;
 
-	if (!*needle) 
+	if (!*needle) /* empty needle */
 		return (haystack);
 	while (*haystack)
 	{
-		if (*haystack == *needle) 
+		if (*haystack == *needle) /* first ch match */
 		{
 			occurance = haystack;
 			tempNeedle = needle;
-			while (*tempNeedle) 
+			while (*tempNeedle) /* check if match */
 			{
 				if (*haystack++ != *tempNeedle++)
-				{ 
+				{ /* not a match, reset pointer to first occurance */
 					haystack = occurance;
 					break;
 				}
 			}
-			if (occurance != haystack) 
+			if (occurance != haystack) /* didn't reset, found match */
 				return (occurance);
 		}
 		haystack++;
 	}
-	return (0); 
+	return (0); /* didn't find match */
 }
